0930f67505d5ad6a468cfc701c97ae8029a6bd37
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-05-10 11:38:53 +0500

    Building Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-center bg-no-repeat bg-cover  bg-hero-pattern">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
