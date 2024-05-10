2bb2b53ed8baf416d8cba6af50546e7e6ba1cad0
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-05-10 18:17:53 +0500

    Buldng logo

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,18 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className="bg-center bg-no-repeat bg-cover  bg-hero-pattern">
+        <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
           <Navbar />
           <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech/>
+        <Works/>
+        <Feedbacks />
+        <div className="relative z-0 ">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
 
-export default Navbar
+  return (
+    <nav
+      className={`{styles.paddindx}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
+    >
+      <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+        <Link
+          to="/"
+          className="flex items-center gap-2 "
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="object-contain w-20 h-12" />
+          <p className=" text-white text-[18px] font-bold cursor-pointer">Abdul <span>| Rehman</span></p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
